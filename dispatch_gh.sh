67c3a0bdbc3cb7f3fadf7ec1b9ac9927c33ba427
Julien Marrec <julien.marrec@gmail.com>
2024-05-06 13:04:31 +0200

    Use main before merging

--- dispatch_gh.sh
+++ dispatch_gh.sh
@@ -3,4 +3,4 @@ gh workflow -R jmarrec/OpenStudio-Tester run manual_installer_test.yml \
     -f os_version=3.8.0 \
     -f os_prerelease_tag=-rc2 \
     -f os_build_sha=ca1c536250 \
-    --ref ruby3.2.2
+    --ref main
