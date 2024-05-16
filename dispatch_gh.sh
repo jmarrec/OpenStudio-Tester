483be6a9800267511d2d89d2f95c621a974689b5
Julien Marrec <julien.marrec@gmail.com>
2024-05-16 14:24:53 +0200

    Track 3.8.0-rc3 on github

--- dispatch_gh.sh
+++ dispatch_gh.sh
@@ -1,6 +1,6 @@
 gh workflow -R jmarrec/OpenStudio-Tester run manual_installer_test.yml \
-    -f base_url=http://openstudio-ci-builds.s3-website-us-west-2.amazonaws.com/3.8.0-rc2 \
+    -f base_url=https://github.com/NREL/OpenStudio/releases/download/v3.8.0-rc3 \
     -f os_version=3.8.0 \
-    -f os_prerelease_tag=-rc2 \
-    -f os_build_sha=ca1c536250 \
+    -f os_prerelease_tag=-rc3 \
+    -f os_build_sha=d8eac889d0 \
     --ref main
